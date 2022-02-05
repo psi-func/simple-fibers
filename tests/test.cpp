dd3d190cf466b6c37440b91fd6834f70d914f1e3
psi-func <shalaginmx998@yandex.ru>
2022-02-05 12:36:55 +0300

    cond_var tested

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,7 @@
 #include <psi/fiber/api.hpp>
 
 #include <psi/fiber/sync/mutex.hpp>
+#include <psi/fiber/sync/condition_variable.hpp>
 
 #include <iostream>
 #include <cassert>
@@ -19,6 +20,9 @@ size_t fibbo(int n)
 
 static int counter = 0;
 psi::fiber::mutex mutex;
+psi::fiber::condition_variable not_empty_;
+
+std::vector<std::size_t> container;
 
 int main()
 {
@@ -28,20 +32,32 @@ int main()
 
                       auto handler1 = spawn([]()
                                            {
-                                               ::mutex.lock();
+                                               self::yield();
                                                std::cout << "Hello from fiber " << self::get_id() << std::endl
                                                          << std::flush;
-                                                self::yield();
+                                                ::mutex.lock();
                                                auto f = fibbo(40);
                                                assert(f == 165580141);
+                                               container.push_back(f);
+                                               ::mutex.unlock();
+                                               not_empty_.notify_one();
+                                               
                                                std::cout << f << std::endl
                                                          << std::flush;
-                                                ::mutex.unlock();
+                                                
                                            });
 
                         auto handler2 = spawn([]() {
                             ::mutex.lock();
-                            std::cout << "from fiber " << self::get_id() << std::endl;
+                            
+                            while (::container.empty())
+                            {
+                                std::cout << "waiting for numbers!!" << std::endl << std::flush;
+                                ::not_empty_.wait(::mutex);
+                            }
+                            
+                            std::cout << "from fiber " << self::get_id() << " " << ::container.back() << std::endl;
+                            ::container.pop_back();
                             ::mutex.unlock();
 
                         });
